2f31c52a54caf3633199e879fa6e7719e944fb37
SSG\sangnd <sangnd@ssg.vn>
2021-06-17 09:23:14 +0700

    Oracle-DinhSang: third week, answer 1, 2, 3 paractice

---
+++ ThirdWeek/Practice/Query.sql
@@ -0,0 +1,53 @@
+SELECT NHAN_VIEN.HO || ' ' || NHAN_VIEN.DEM || ' ' || NHAN_VIEN.TEN AS "QL"
+FROM NHAN_VIEN
+WHERE NHAN_VIEN_ID = (SELECT CAP_TREN_ID
+                    FROM QUAN_LY
+                    WHERE CAP_DUOI_ID = 'SSG2');
+                    
+                    
+SELECT NHAN_VIEN.HO || ' ' || NHAN_VIEN.DEM || ' ' || NHAN_VIEN.TEN AS "QL", QUAN_LY.CAP_DUOI_ID
+FROM (QUAN_LY
+        LEFT JOIN NHAN_VIEN ON NHAN_VIEN_ID = CAP_TREN_ID) 
+                                        
+
+SELECT NHAN_VIEN_ID AS "NV", LUONG
+FROM LUONG, (SELECT NHAN_VIEN_ID AS NVID, MAX(LUONG_TU_NGAY) AS NGAY
+                        FROM LUONG
+                        GROUP BY NHAN_VIEN_ID)
+WHERE NHAN_VIEN_ID = NVID AND LUONG_TU_NGAY = NGAY;
+                                                                                       
+SELECT PHONG_BAN.TEN_PHONG_BAN AS "PB",
+        NHAN_VIEN.NHAN_VIEN_ID AS "MNV",
+        NHAN_VIEN.HO || ' ' || NHAN_VIEN.DEM || ' ' || NHAN_VIEN.TEN AS "HTNV",
+        CHUC_VU.TEN_CHUC_VU AS "CV",
+        NHAN_VIEN.NGAY_SINH AS "NS"
+FROM ((NHAN_VIEN
+    LEFT JOIN PHONG_BAN ON NHAN_VIEN.PHONG_BAN_ID = PHONG_BAN.PHONG_BAN_ID)
+    LEFT JOIN CHUC_VU ON NHAN_VIEN.CHUC_VU_ID = CHUC_VU.CHUC_VU_ID)
+  
+ 
+ 
+ -- Câu 3 (STT chưa làm được)
+ SELECT PB AS "Phòng Ban", MNV AS "Mã Nhân Viên", HTNV AS "Họ Tên Nhân Viên", CV AS "Chức Vụ", QL AS "Người Quản Lý", LUONG AS "Mức Lương", TO_CHAR(NS, 'fmDD/MM/yyyy') AS "Ngày Sinh"
+ FROM (((   SELECT PHONG_BAN.TEN_PHONG_BAN AS "PB",
+                    NHAN_VIEN.NHAN_VIEN_ID AS "MNV",
+                    NHAN_VIEN.HO || ' ' || NHAN_VIEN.DEM || ' ' || NHAN_VIEN.TEN AS "HTNV",
+                    CHUC_VU.TEN_CHUC_VU AS "CV",
+                    NHAN_VIEN.NGAY_SINH AS "NS"
+            FROM ((NHAN_VIEN
+                    LEFT JOIN PHONG_BAN ON NHAN_VIEN.PHONG_BAN_ID = PHONG_BAN.PHONG_BAN_ID)
+                    LEFT JOIN CHUC_VU ON NHAN_VIEN.CHUC_VU_ID = CHUC_VU.CHUC_VU_ID))
+            LEFT JOIN (SELECT NHAN_VIEN_ID AS "NV", LUONG
+                        FROM LUONG, (SELECT NHAN_VIEN_ID AS NVID, MAX(LUONG_TU_NGAY) AS NGAY
+                                        FROM LUONG
+                                        GROUP BY NHAN_VIEN_ID)
+                        WHERE NHAN_VIEN_ID = NVID AND LUONG_TU_NGAY = NGAY) ON MNV = NV)
+            LEFT JOIN (SELECT NHAN_VIEN.HO || ' ' || NHAN_VIEN.DEM || ' ' || NHAN_VIEN.TEN AS "QL", QUAN_LY.CAP_DUOI_ID
+                        FROM (QUAN_LY
+                            LEFT JOIN NHAN_VIEN ON NHAN_VIEN_ID = CAP_TREN_ID)) ON MNV = CAP_DUOI_ID)
+ORDER BY MNV;
+        
+        
+        
+    
+    
